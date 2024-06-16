3507a22584d6bce8b0bb851716c43027d35a6deb
Samgrass <samuelegrassetti01@gmail.com>
2024-06-16 18:33:57 +0200

    aggiunto creazione di 16 numeri random da 1 a 100 + aggiunto condizione per dare le classi "red" e "blue"

--- js/myscript.js
+++ js/myscript.js
@@ -8,23 +8,40 @@ btnPlay.addEventListener ('click',
         squareContainer.innerHTML = '';
         // aggiungo bordo alla griglia
         squareContainer.classList.add("black-border");
+        // creo 16 bombe da inserire casualmente nella griglia
+        const bomb = []
+
+        while (bomb.length < 16) {
+
+            // creo numero random da 1 a 100
+            let randomNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+            // evito che esca lo stesso numero
+            if (!bomb.includes(randomNumber)) {
+                bomb.push(randomNumber)
+            }
+        }
+        console.log(bomb);
         // ciclo da 100 giri
         for (let i = 1; i <= 100; i++) {
             // creo un elemento "quadrato"
             let square = document.createElement("div");
             square.classList.add("square");
             square.append(i)
-            
+
             // aggiungo click all'elemento per dare classe e far apparire il numero index
             square.addEventListener('click', 
                 function () {
                     square.classList.toggle("clicked");
-                    
-                    squareText = i
-                    console.log(squareText);
+                    console.log(i);
                 }
             )
-            console.log(square);
+            
+            if (bomb.includes(i)) {
+                square.classList.add("red")  
+            } else {
+                square.classList.add("blue")  
+            }
+            
             squareContainer.append(square);
         } 
     }
