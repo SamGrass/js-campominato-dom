b8045ab296dfdba46a4412f8678bbf94a933a25e
Samgrass <samuelegrassetti01@gmail.com>
2024-10-30 11:41:26 +0100

    fix click prevent at game's end

--- js/myscript.js
+++ js/myscript.js
@@ -6,6 +6,8 @@ const result = document.querySelector("h2");
 // creo array per sapere quante celle blu sono state cliccate
 let squareBlue = document.getElementsByClassName("blue clicked");
 let squareRed = document.getElementsByClassName("square red clicked");
+
+let gameOver = false;
 // aggiungo evento click 
 btnPlay.addEventListener ('click',
     function () {
@@ -13,11 +15,12 @@ btnPlay.addEventListener ('click',
         result.innerHTML = '';
         // aggiungo bordo alla griglia
         squareContainer.classList.add("black-border");
+        let gameOver = false;
         // creo 16 bombe da inserire casualmente nella griglia
         const bomb = []
 
         while (bomb.length < 16) {
-
+            
             // creo numero random da 1 a 100
             let randomNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1)
             // evito che esca lo stesso numero
@@ -25,29 +28,28 @@ btnPlay.addEventListener ('click',
                 bomb.push(randomNumber)
             }
         }
-        console.log(bomb);
+
         // ciclo da 100 giri
         for (let i = 1; i <= 100; i++) {
             // creo un elemento "quadrato"
             let square = document.createElement("div");
             square.classList.add("square");
             square.append(i)
-
-            // do le classi in base alla posizione delle bombe
-            if (bomb.includes(i)) {
-                square.classList.add("red");  
-            } else {
-                square.classList.add("blue");  
-            }
             // aggiungo click all'elemento per dare classe e far apparire il numero index
             square.addEventListener('click', 
                 function () {
+                    if (gameOver || square.classList.contains("clicked")) return;
                     square.classList.add("clicked");
                     // se viene cliccata una bomba faccio vedere il punteggio
-                    if (squareRed.length === 1) {
-                        result.append(`Hai perso, il tuo punteggio è: ${squareBlue.length}`)
-                    }  else if (squareBlue.length === 84) {
-                        result.append(`Hai vinto, il tuo punteggio è: ${squareBlue.length}`)
+                    if (bomb.includes(i)) {
+                        square.classList.add("red");
+                        result.append(`Hai perso, il tuo punteggio è: ${squareBlue.length}. Premi gioca per ricominciare`);
+                        gameOver = true;
+                    } else if (!bomb.includes(i)) {
+                        square.classList.add("blue");
+                    } else if (squareBlue.length === 84) {
+                        result.append(`Hai vinto, il tuo punteggio è: ${squareBlue.length}. Premi gioca per ricominciare`);
+                        gameOver = true;
                     }
                 }
             )
